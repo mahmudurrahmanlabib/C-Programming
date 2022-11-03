02be5e5f23830af1ff16e3b6192ae112afa9688c
Mahmudur Rahman Labib <labibrahman555@gmail.com>
2022-11-03 12:43:08 +0600

    Update 1.c

--- Lab_Work_4/1.c
+++ Lab_Work_4/1.c
@@ -1,26 +1,53 @@
-
 #include<stdio.h>
 
-int main()
+void main()
+
 {
-        int n,sum,sum1=0,i,j;
 
-        
-        /* For The Pattern The Formula Would Be [{n(n+1)(n+2)}/6] */
-        printf("\nEnter value for n = ");
-        scanf("%d",&n);
+   int i,j,n,sum=0,sumin=0;
 
-        for(i=1;i<=n;i++)
-    {
-                sum=0;
-                for(j=1;j<=i;j++)
-                {
-                    sum=sum+j;
-                }
-                sum1=sum1+sum;
-        }
+   printf("Enter the value of n = ");
 
-        printf("\nThe Sum of Series up to Value [ %d ] = [ %d ]\n",n,sum1);
+   scanf("%d",&n);
 
-        return 0;
+   for(i=1; i<=n; i=i+1)
+
+   {
+
+   printf("(");
+
+      for(j=1;j<=i;j=j+1)
+
+      {
+
+        sumin=sumin+j;
+
+        printf("%d",j);
+
+        if (j<i)
+
+        printf("+");
+
+        else if(j=i)
+
+        printf(")");
+
+      }
+
+      sum=sum+sumin;
+
+       sumin=0;
+
+        if (i<n)
+
+        printf("+");
+
+        else if(i=n)
+
+        printf(" ");
+
+   }
+
+   printf("= %d\n",sum);
+
 }

e1f6c247b820df10f3c1b734f8bdfc358d2ec5d4
hrithikacharjee <113775653+hrithikacharjee@users.noreply.github.com>
2022-11-03 12:17:07 +0600

    Update 3b.c

--- Lab_Work_4/3b.c
+++ Lab_Work_4/3b.c
@@ -1,21 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
 #include<conio.h>
 int main()
 {
-int n, i, j, k;
-printf("Enter the number of rows: ");
-scanf("%d",&n);
-for(i = 1; i <= n; i++)
-{
-for(j = i; j <n; j++)
-{
-printf(" ");
-}
-for(k = 1; k < (i*2); k++)
-{
-printf("%d",k);
-}
-printf("\n");
-}
-return 0;
+    int i, space, rownum, k=0;
+    printf("Enter the number of rows : ");
+    scanf("%d", &rownum);
+    for(i=1; i<=rownum; i++)
+    {
+        for(space=1; space<=(rownum-i); space++)
+            printf("  ");
+        while(k!=(2*i-1))
+        {
+            printf("* ");
+            k++;
+        }
+        k=0;
+        printf("\n");
+    }
+    getch();
+    return 0;
 }

bb12bd3a6e8edc1972f7a64933adde786dbe4075
Mahmudur Rahman Labib <labibrahman555@gmail.com>
2022-12-02 22:00:07 +0600

    Add files via upload

---
+++ Assignment_Array/1_Sum_Avg.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<math.h> //for sqrt function
+void func(int a, int b, int c, int d, int e, float *sum, float *avg, float *std_dev);
+
+//main function
+void main()
+{
+    int a,b,c,d,e;
+    float sum, avg;
+
+    printf("Enter 1st number: ");
+    scanf("%d", &a);
+    printf("Enter 2nd number: ");
+    scanf("%d", &b);
+    printf("Enter 3rd number: ");
+    scanf("%d", &c);
+    printf("Enter 4th number: ");
+    scanf("%d", &d);
+    printf("Enter 5th number: ");
+    scanf("%d", &e);
+
+    func(a,b,c,d,e,&sum,&avg);
+
+    printf("The Sum: %f\n", sum);
+    printf("The Average: %f\n", avg);
+
+}
+
+//func function
+void func(int a, int b, int c, int d, int e, float *sum, float *avg, float *std_dev)
+{
+    *sum = a+b+c+d+e;
+    *avg = *sum/5.0;
+
+}
---
+++ Assignment_Array/5_Fibonacci_Sequence.c
@@ -0,0 +1,16 @@
+    #include <stdio.h> 
+     
+    int main() 
+     
+    { 
+    	int arr[3]={0,1,0},n; 
+    	n=10;//user input 
+    	 
+    // series formation 
+    	for(int i=3;i<=n+3;i++)	 
+    	  
+    printf("\n%d",arr[i%3]=arr[(i+1)%3]+arr[(i+2)%3]);	  
+    	 
+    	 
+    	return 0; 
+    	} 
